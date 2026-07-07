2cc9fe7c9b5d7d04bd58c60775e82eab9c51a9eb
Elgember Juracan <elgemberj@gmail.com>
2026-07-07 10:08:32 -0600

    solucionado error para vergel

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-const root = document.getElementById('root') as HTMLElement
+const root = document.getElementById('root') as HTMLElement;
 createRoot(root).render(
   <React.StrictMode>
       <App />

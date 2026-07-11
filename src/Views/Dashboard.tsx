c07e3803600a65dc48ecaa9c220c14f03a027af2
Elgember Juracan <elgemberj@gmail.com>
2026-07-11 14:33:09 -0600

    aliniando el componente dashbord

--- src/Views/Dashboard.tsx
+++ src/Views/Dashboard.tsx
@@ -7,16 +7,18 @@ import { authContext } from "@/Context/AuthContext";
 export const Dashboard = () => {
     const { user } = authContext();
     return (
-    <div className="p-10 bg-white dark:bg-[#333] text-black dark:text-white min-h-screen">
-        <h1>Bienvenido a dashboard</h1>
-        <ThemeToggle />
-        <Card user={user ?? undefined} />
+    <div className="p-10 bg-white dark:bg-[#333] text-black dark:text-white min-h-full flex justify-center">
         <div>
-            {user?.role === 'admin' ? (
-                <IrToAdminPanel />
-            ) : (
-                null
-            )}
+            <h1 className="text-center py-2">Bienvenido a dashboard</h1>
+            <ThemeToggle />
+            <Card user={user ?? undefined} />
+            <div>
+                {user?.role === 'admin' ? (
+                    <IrToAdminPanel />
+                ) : (
+                    null
+                )}
+            </div>
         </div>
     </div>
     )
